95abed7195ea3e85c603d8c4b668334360fbe5c6
ICodeGirl <95872325+Shreya24Patel@users.noreply.github.com>
2025-03-17 12:34:34 +0530

    Update settings.py

--- ec/ec/settings.py
+++ ec/ec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-70_a%bs))&l63!7y(8-7y8vtcb(3386nceu+_dgwbdf+1leuid
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
